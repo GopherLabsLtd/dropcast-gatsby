8fdb33cfa4b28048dea05922c2678910b2a75220
The Iliad <iliadmoosavi@gmail.com>
2018-05-22 22:57:59 -0400

    Added gatsby-sass-plugin, Sass & image files

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import '../styles/main.scss'
 import { rhythm, scale } from '../utils/typography'
 
 class Template extends React.Component {
